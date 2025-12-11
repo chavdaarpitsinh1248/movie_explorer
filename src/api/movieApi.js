44051afac716dbb4b6506b46b34c757f331f7421
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-11 19:17:23 +0530

    filter for year and type

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -1,7 +1,10 @@
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 export async function searchMovies(query) {
-    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`;
+    let url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`;
+    if (year) url += `&y=${year}`;
+    if (type) url += `&type=${type}`;
+    
     const response = await fetch(url);
     const data = await response.json();
     return data.Search || [];
