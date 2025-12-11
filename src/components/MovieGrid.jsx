450da218a8951bd46388b00b4af086c53f5bb30e
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-11 19:41:23 +0530

    movie grid 5 cards in large screen

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,11 +1,11 @@
 import MovieCard from "./MovieCard";
 
 export default function MovieGrid({ movies }) {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-            {movies.map((movie) => (
-                <MovieCard key={movie.imdbID} movie={movie} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+      {movies.map((movie) => (
+        <MovieCard key={movie.imdbID} movie={movie} />
+      ))}
+    </div>
+  );
 }
