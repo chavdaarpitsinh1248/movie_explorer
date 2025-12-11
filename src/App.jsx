d5768e80e8a2cbdb2db2d81641f1c5eb3e043143
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-11 16:35:31 +0530

    Implement main application structure with routing and layout components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Watchlist from "./pages/Watchlist";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <main className="min-h-screen px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
