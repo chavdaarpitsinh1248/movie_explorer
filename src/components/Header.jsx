7d5f21e853fcdc5e743b14612b4e426e74aee660
Chavda Arpitsinh <chavdaarpitsinh1248@gmail.com>
2025-12-11 16:42:34 +0530

    Add Footer, Header, and Home components with basic structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="bg-black text-white px-6 py-4">
+            <h1 className="text-2xl font-bold">Movie Explorer</h1>
+        </header>
+    );
+}
